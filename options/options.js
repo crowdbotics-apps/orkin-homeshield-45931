a424e28c8188f06f3e1f4ab81934349d76cc8683
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:09:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orkin-homeshield-45931",
+  url: "https://orkin-homeshield-45931.botics.co",
+  api: "https://orkin-homeshield-45931.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
